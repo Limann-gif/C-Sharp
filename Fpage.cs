062a9f894cccfc08a58edea2a8c9ddf6ba09de06
Sammiat <sammiatlimann@gmail.com>
2021-11-04 23:54:44 +0000

    First Commit

---
+++ Fpage.cs
@@ -0,0 +1,12 @@
+var Name = "Bob";
+var Numb = 3;
+var Temp = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(Name);
+Console.Write("! You have ");
+Console.Write(Numb);
+Console.Write(" in you inbox. ");
+Console.Write("The temperature is "); 
+Console.Write(Temp);
+Console.Write(" celcius");
